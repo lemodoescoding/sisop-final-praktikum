3bc2d2cabef24edf0784c2cec72456a0382edf19
qbonafide <177829341+qbonafide@users.noreply.github.com>
2025-06-27 20:18:22 +0700

    ysee

--- src/std_lib.c
+++ src/std_lib.c
@@ -10,31 +10,38 @@ int div(int a, int b) {
 }
 
 int mod(int a, int b) {
-  while (a >= b) a -= b;
+  while (a >= b)
+    a -= b;
   return a;
 }
 
-void memcpy(byte* dst, byte* src, unsigned int size) {
+void memcpy(byte *dst, byte *src, unsigned int size) {
   unsigned int i;
-  for (i = 0; i < size; i++) dst[i] = src[i];
+  for (i = 0; i < size; i++)
+    dst[i] = src[i];
 }
 
-unsigned int strlen(char* str) {
+unsigned int strlen(char *str) {
   unsigned int len = 0;
-  while (str[len] != '\0') len++;
+  while (str[len] != '\0')
+    len++;
   return len;
 }
 
-bool strcmp(char* str1, char* str2) {
+bool strcmp(char *str1, char *str2) {
   unsigned int i = 0;
+
   while (str1[i] != '\0' && str2[i] != '\0') {
-    if (str1[i] != str2[i]) return false;
+    if (str1[i] != str2[i])
+      return 0;
+
     i++;
   }
-  return str1[i] == str2[i];
+
+  return str1[i] == '\0' && str2[i] == '\0';
 }
 
-void strcpy(char* dst, char* src) {
+void strcpy(char *dst, char *src) {
   unsigned int i = 0;
   while (src[i] != '\0') {
     dst[i] = src[i];
@@ -43,7 +50,8 @@ void strcpy(char* dst, char* src) {
   dst[i] = '\0';
 }
 
-void clear(byte* buf, unsigned int size) {
+void clear(byte *buf, unsigned int size) {
   unsigned int i;
-  for (i = 0; i < size; i++) buf[i] = 0;
+  for (i = 0; i < size; i++)
+    buf[i] = 0;
 }
